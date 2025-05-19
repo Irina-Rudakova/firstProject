931efce2eec8e9f2387710ef6af489d6a109c45f
Irina Rudakova <irina44.photographer@gmail.com>
2025-05-19 21:32:19 +0300

    Add files via upload

---
+++ first-project/js-info.js
@@ -0,0 +1,6 @@
+alert("Язык программирования JavaScript разработал Брэндан Эйх.");
+alert("JavaScript был разработан за 10 дней.");
+alert("JavaScript появился в 1995 году.");
+alert("JavaScript — это не Java.");
+alert("Спецификация для JavaScript называется ECMAScript.");
+
